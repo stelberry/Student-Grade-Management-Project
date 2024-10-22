f6fc016ce195dfce5434d38d6d42064629a097e9
Yoon Phyo Ei <WMIS161@live.rhul.ac.uk>
2024-10-22 06:49:03 +0100

    added NoGradeAvailableException.java

---
+++ CW1/CourseWork1/src/main/java/uk/ac/rhul/cs2800/exception/NoGradeAvailableException.java
@@ -0,0 +1,10 @@
+package uk.ac.rhul.cs2800.exception;
+
+/**
+ * Exception for student class if there is no grates.
+ */
+public class NoGradeAvailableException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+}
