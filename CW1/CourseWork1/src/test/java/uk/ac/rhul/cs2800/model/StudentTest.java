2b2250f70328f77c43f3b67cadd5af8cef40a410
Yoon Phyo Ei <WMIS161@live.rhul.ac.uk>
2024-10-23 08:57:18 +0100

    modified StudentTest class

--- CW1/CourseWork1/src/test/java/uk/ac/rhul/cs2800/model/StudentTest.java
+++ CW1/CourseWork1/src/test/java/uk/ac/rhul/cs2800/model/StudentTest.java
@@ -4,23 +4,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import uk.ac.rhul.cs2800.exception.NoGradeAvailableException;
+import uk.ac.rhul.cs2800.exception.NoRegistrationException;
 
 public class StudentTest {
 
   @Test
-  void computeAverageTest() throws NoGradeAvailableException {
+  void computeAverageTest() throws NoGradeAvailableException, NoRegistrationException {
     // Test 1
     Student student = new Student();
-    student.addGrade(new Grade(5));
-    student.addGrade(new Grade(5));
-    student.addGrade(new Grade(5));
+    Grade grade1 = new Grade(80);
+    Grade grade2 = new Grade(90);
+    Module module1 = new Module(101, "Software Engineering", true);
+    Module module2 = new Module(102, "Databases", false);
+    
+    student.registerModule(module1);
+    student.registerModule(module2);
 
+    student.addGrade(grade1, module1);
+    student.addGrade(grade2, module2);
     assertEquals(5.0f, student.computeAverage());
 
   }
 
   @Test
-  void computeAverageTestwithEmptyGrades() {
+  void computeAverageTestwithEmptyGradesTest() {
     // Test 2
     assertThrows(NoGradeAvailableException.class, () -> {
       Student student = new Student();
@@ -28,26 +35,26 @@ public class StudentTest {
     });
   }
   
-}
-
-  void addGradeTest() throws NoGradeAvailableException {
+  @Test
+  void addGradeTest() throws NoGradeAvailableException, NoRegistrationException {
     // Test 5: Ensure grades can be added and retrieved properly.
     Student student = new Student();
-    Grade grade = new Grade(7);
-    student.addGrade(grade);
+    Module module = new Module(101, "Software Engineering", true);
+    Grade grade = new Grade(80);
+    student.addGrade(grade, module);
+    assertEquals(8, student.getGrade(module).getScore());
 
-    assertEquals(7, grade);
   }
 
   @Test
-  void getGradeTest() throws NoGradeAvailableException {
+  void getGradeTest() throws NoGradeAvailableException, NoRegistrationException {
     // Test 6
     Student student = new Student();
     Module module = new Module(101, "Software Engineering", true);
     Grade grade = new Grade(80);
     student.addGrade(grade);
 
-    student.registeraterModule(module);
+    student.registerModule(module);
 
     assertEquals(80, student.getGrade(module).getScore());
   }
