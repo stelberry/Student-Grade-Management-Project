4f7ff7fdc64ded0f6341fd89e61e2e93f3e110ee
Yoon Phyo Ei <WMIS161@live.rhul.ac.uk>
2024-10-23 06:15:23 +0100

    modified ModuleTest class

--- CW1/CourseWork1/src/test/java/uk/ac/rhul/cs2800/model/ModuleTest.java
+++ CW1/CourseWork1/src/test/java/uk/ac/rhul/cs2800/model/ModuleTest.java
@@ -1,12 +1,20 @@
 package uk.ac.rhul.cs2800.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class ModuleTest {
 
+  @Test
   void moduleTest() {
     // Test6
     Module module = new Module();
     module.setCode(101);
     module.setName("Software Engineering");
     module.setMnc(true);
+
+    assertEquals(101, module.getCode());
+    assertEquals("Software Engineering", module.getName());
+    assertEquals(true, module.isMnc());
   }
 }
