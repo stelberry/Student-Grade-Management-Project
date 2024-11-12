1ce9ce3307dc246dc8aa018fce9757da55b36465
Yoon Phyo Ei <WMIS161@live.rhul.ac.uk>
2024-11-12 00:38:33 +0000

    #4 add new Maven project for CW2

---
+++ CW2/src/main/java/uk/ac/rhul/cs2800/Cw2Application.java
@@ -0,0 +1,16 @@
+package uk.ac.rhul.cs2800;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main Application class.
+ */
+@SpringBootApplication
+public class Cw2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Cw2Application.class, args);
+  }
+
+}
