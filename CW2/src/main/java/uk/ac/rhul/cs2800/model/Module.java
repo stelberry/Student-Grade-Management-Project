8b80090dff4c7c96adea7c5f78a232258b56182c
Ei Yoon (2023) WMIS161 <yoon.ei.2023@live.rhul.ac.uk>
2024-11-24 12:51:05 +0000

    #6 New maven project

--- CW2/src/main/java/uk/ac/rhul/cs2800/model/Module.java
+++ CW2/src/main/java/uk/ac/rhul/cs2800/model/Module.java
@@ -1,7 +1,6 @@
 package uk.ac.rhul.cs2800.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
