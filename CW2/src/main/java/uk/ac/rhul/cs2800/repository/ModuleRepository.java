aacdf3f2461fe1e9052e0f38827860da131d97c9
Yoon Phyo Ei <WMIS161@live.rhul.ac.uk>
2024-11-29 00:49:08 +0000

    Revert "Merge branch 'revert-44390855' into 'main'"
    This reverts commit 3ae559628c340b7e044227a6b45f32f848e43baf, reversing
    changes made to 443908556a90665a1424c2e4b218d87c38b7b592.

--- CW2/src/main/java/uk/ac/rhul/cs2800/repository/ModuleRepository.java
+++ CW2/src/main/java/uk/ac/rhul/cs2800/repository/ModuleRepository.java
@@ -3,6 +3,9 @@ package uk.ac.rhul.cs2800.repository;
 import org.springframework.data.repository.CrudRepository;
 import uk.ac.rhul.cs2800.model.Module;
 
+/**
+ * A repository manages module repository.
+ */
 public interface ModuleRepository extends CrudRepository<Module, String> {
 
 }
