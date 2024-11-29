23809773fa9e01ddbe026bc555d63cc0163284cd
Yoon Phyo Ei <WMIS161@live.rhul.ac.uk>
2024-11-29 03:07:51 +0000

    Revert "Merge branch 'newMavenProject' into 'main'"
    This reverts commit 17f9c14582cdb91d5cc00a6bcc99a0ef67b9644f, reversing
    changes made to d86ea9b12dc6db8246be190e3dee089b86e3bf1e.

--- CW2/src/main/java/uk/ac/rhul/cs2800/repository/GradeRepository.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.rhul.cs2800.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import uk.ac.rhul.cs2800.model.Grade;
-
-public interface GradeRepository extends CrudRepository<Grade, Long> {
-
-}
