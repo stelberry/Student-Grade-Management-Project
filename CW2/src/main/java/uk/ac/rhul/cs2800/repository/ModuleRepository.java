964959b0b3adfe59f1a0e168adf3140a4b29ab3c
Yoon Phyo Ei <WMIS161@live.rhul.ac.uk>
2024-11-29 02:15:59 +0000

    Revert "Merge branch 'main' of https://gitlab.cim.rhul.ac.uk/wmis161/CS2800"
    This reverts commit 90fea894b08126e46ee2f3f6cace68aa5cd19466, reversing
    changes made to 0de1f36078dc0b5043ef044a8fb4050f17c8f0de.

--- CW2/src/main/java/uk/ac/rhul/cs2800/repository/ModuleRepository.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.rhul.cs2800.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import uk.ac.rhul.cs2800.model.Module;
-
-
-public interface ModuleRepository extends CrudRepository<Module, Long> {
-
-}
