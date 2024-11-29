dd744eec8c622d56d564bd64774eff9c576dbb13
Yoon Phyo Ei <WMIS161@live.rhul.ac.uk>
2024-11-29 01:24:12 +0000

    Revert "Merge branch 'newMavenProject' into 'main'"
    This reverts commit e05336ffd8563fa67544fac159fbdd6fbc5d98f5, reversing
    changes made to 36422ada86b096817ce8cf00590670ac8b78cd5f.

--- CW2/src/main/java/uk/ac/rhul/cs2800/model/Module.java
+++ CW2/src/main/java/uk/ac/rhul/cs2800/model/Module.java
@@ -1,6 +1,7 @@
 package uk.ac.rhul.cs2800.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
--- CW2/src/test/java/uk/ac/rhul/cs2800/controller/GradeControllerTest.java
+++ CW2/src/test/java/uk/ac/rhul/cs2800/controller/GradeControllerTest.java
@@ -1,7 +1,4 @@
 package uk.ac.rhul.cs2800.controller;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,72 +6,45 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import uk.ac.rhul.cs2800.model.Grade;
 import uk.ac.rhul.cs2800.model.Module;
 import uk.ac.rhul.cs2800.model.Student;
 import uk.ac.rhul.cs2800.repository.ModuleRepository;
 import uk.ac.rhul.cs2800.repository.StudentRepository;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-
 public class GradeControllerTest {
-
   @Autowired
   private MockMvc mockMvc;
-
   @Autowired
   private ObjectMapper objectMapper;
-
   @Autowired
   private StudentRepository studentRepository;
-
   @Autowired
   private ModuleRepository moduleRepository;
-
   Student student;
-
   Module module;
-
   @BeforeEach
   void beforeEach() {
     student = new Student();
     student.setId(1010L);
-    student.setFirstName("Yoon");
     student = studentRepository.save(student);
-
-    module = new Module();
-    module.setCode("CS2855");
-    module.setName("databases");
-    module = moduleRepository.save(module);
   }
-
   @Test
   void addGradeTest() throws JsonProcessingException, Exception {
-    Map<String, String> params = new HashMap<String, String>();
-    params.put("student_id", String.valueOf(student.getId()));
-    params.put("module_code", module.getCode());
-    params.put("score", "50");
-
+    Map<String, Integer> params = new HashMap<String, Integer>();
+    params.put("student_id", 1);
+    params.put("score", 4);
+    params.put("module_id", 1);
     MvcResult action = mockMvc.perform(MockMvcRequestBuilders.post("/grades/addGrade")
         .contentType(MediaType.APPLICATION_JSON)
         .content(objectMapper.writeValueAsString(params))
         .accept(MediaType.APPLICATION_JSON)
         ).andReturn();
-        assertEquals(HttpStatus.OK.value(), action.getResponse().getStatus());
-
-        Grade grade =
-            objectMapper.readValue(action.getResponse().getContentAsString(), Grade.class);
-        assertEquals(student.getId(), grade.getStudent().getId());
-        assertEquals(module.getCode(), grade.getModule().getCode());
-        assertEquals(50, grade.getScore());
-        assertNotNull(grade.getId());
   }
 }
