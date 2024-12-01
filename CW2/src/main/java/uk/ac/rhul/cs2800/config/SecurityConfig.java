3cc8b7bcabf0fe2b4fb6ed2a15741d622639e4e9
Yoon Phyo Ei <WMIS161@live.rhul.ac.uk>
2024-12-01 19:30:37 +0000

    #13 update pom.xml and add SecurityConfig class

---
+++ CW2/src/main/java/uk/ac/rhul/cs2800/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package uk.ac.rhul.cs2800.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+@Configuration
+public class SecurityConfig {
+
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf((csrf) -> csrf.disable()).cors(withDefaults());
+
+    return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOriginPatterns(Arrays.asList("*"));
+    config.setAllowedHeaders(Arrays.asList("*"));
+    config.setAllowedMethods(Arrays.asList("*"));
+    config.setAllowCredentials(false);
+    config.applyPermitDefaultValues();
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return source;
+  }
+}
