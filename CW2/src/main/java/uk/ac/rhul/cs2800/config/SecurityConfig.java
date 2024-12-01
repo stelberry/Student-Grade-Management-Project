aefab76b8fa9decab26f5693eb32e41571a9124f
Yoon Phyo Ei <WMIS161@live.rhul.ac.uk>
2024-12-01 19:33:26 +0000

    #13 add checkstyle for SecurityConfig class

--- CW2/src/main/java/uk/ac/rhul/cs2800/config/SecurityConfig.java
+++ CW2/src/main/java/uk/ac/rhul/cs2800/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package uk.ac.rhul.cs2800.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +11,20 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
+/**
+ * CORS configuration * for managing application security policies.
+ */
 @Configuration
 public class SecurityConfig {
 
-
+  /**
+   * Configures the security filter chain for the application to allow the application to interact
+   * with resources from different origins.
+   *
+   * @param http the HttpSecurity object used to configure security settings
+   * @return the configured SecurityFilterChain
+   * @throws Exception if there is an error during configuration
+   */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf((csrf) -> csrf.disable()).cors(withDefaults());
@@ -22,6 +32,11 @@ public class SecurityConfig {
     return http.build();
   }
 
+  /**
+   * Defines CORS configuration for the application.
+   *
+   * @return the CorsConfigurationSource containing the configured CORS policy
+   */
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
